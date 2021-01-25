50eaa6bd06b772f21d44c96403aea633add0550d
Evgeniy Androsov <ipr9nka@gmail.com>
2021-01-25 10:59:13 +0300

    v0.1

---
+++ AsyncSumN/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncSumN
+{
+    public class Calculator
+    {
+        private async Task<int> SumAction(int n, CancellationToken token)
+        {
+            Console.WriteLine("Идет вычисление");
+            int temp = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                Thread.Sleep(1000);
+                temp += i;
+                if(token.IsCancellationRequested)
+                    return temp;
+            }
+
+            return temp;
+        }
+
+        public async Task<int> SumAsync(int n,CancellationToken token)
+        {
+            var result = await Task.Run(() => SumAction(n, token),token);
+
+            if(!token.IsCancellationRequested)
+            Console.WriteLine(result);
+
+            return result;
+        }
+    }
+}
---
+++ AsyncSumN/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncSumN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            Calculator calculator = new Calculator();
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+            while (true)
+            {
+                if (Int32.TryParse(Console.ReadLine(), out int n))
+                {
+                    tokenSource.Cancel();
+                    tokenSource = new CancellationTokenSource();
+
+                    calculator.SumAsync(n, tokenSource.Token);
+                    Console.WriteLine("Введите другое число для перезагрузки с новыми параметрами");
+                }
+                else
+                    Console.WriteLine("It's not a number");
+            }
+        }
+    }
+}
